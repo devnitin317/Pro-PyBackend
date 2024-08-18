a6657b9f512c6c825b13678c368ad7bfbfaba30e
Nitin <nitinsingh919293@gmail.com>
2024-08-18 10:28:34 +0530

    tenth

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 
 from flask import Flask, jsonify, request
+import nltk
+from language_tool_python import LanguageTool
+from nltk.tokenize import sent_tokenize
 
-
+nltk.download('words')
+nltk.download('punkt')
 app = Flask(__name__)
-
 
 
 # Route to get all items
@@ -17,7 +20,44 @@ def get_name():
     print("hello")
     return "Nitin Singh"
 
+# Route to create a new item
+@app.route('/', methods=['POST'])
+def create_item():
+    data = request.get_json()
+    print(data)
+    
+    grammerMistakes = check_grammar_with_languagetool(data['summary'])
+    
+    extracted_mistakes = []
 
+    for mistake in grammerMistakes:
+        extracted_mistake = {
+            'error': mistake.matchedText, 
+            'replacements': mistake.replacements,
+            'context':mistake.context,
+        }
+        extracted_mistakes.append(extracted_mistake)
+    
+    
+    mistakes = {
+        'grammerMistakes': extracted_mistakes
+    }
+    
+    
+    finalData = jsonify(mistakes)
+    print(finalData)
+    return finalData, 201
+
+
+def check_grammar_with_languagetool(text):
+    mistakes = []
+    tool = LanguageTool('en-US')
+    sentences = sent_tokenize(text)
+    for sentence in sentences:
+        matches = tool.check(sentence)
+        for match in matches:
+            mistakes.append(match)
+    return mistakes  
 
 if __name__ == '__main__':
     app.run(port=1234,debug=True)
